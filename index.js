8bee138f81226178519efa2f1f66ad662e79322a
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-07-10 17:33:50 -0400

    Refactor all code

---
+++ index.js
@@ -0,0 +1,4 @@
+import Knight from "./square.js";
+
+const myKnight = new Knight();
+myKnight.findPath([0, 0], [1, 1]);
--- square.js
+++ square.js
@@ -1,25 +1,41 @@
-export default class Square {  
-  constructor(positionX = null, positionY = null) {
-    this.positionX = positionX;
-    this.positionY = positionY;
-    this.nextMoves = [];
+export default class Knight {  
+  constructor() {
+    this.pastMove = null;
   }
 
   static possibleX = [-2, -2, -1, -1, 1, 1, 2, 2];
   static possibleY = [-1, 1, -2, 2, -2, 2, -1, 1];
 
-  moveList() {
-    const x = this.positionX;
-    const y = this.positionY;
-    let possibleMoves = [];
-    for (let i = 0; i < 8; i++) {
-      // does not exceed lower grid bounds...
-      if (0 <= (x + Square.possibleX[i]) && 0 <= (y + Square.possibleY[i])) {
-        // or upper grid bounds
-        if (8 > (x + Square.possibleX[i]) && 8 > (y + Square.possibleY[i])) {
-          let move = [x + Square.possibleX[i], y + Square.possibleY[i]];
-          possibleMoves.push(move);
-        }
+  findPath(start, end, queue = [start]) {
+
+    let curr = start;
+    // search iteratively by level until a match is found
+    while (queue.length) {
+      curr = queue.shift();
+      if (curr[0] === end[0] && curr[1] === end[1]) break;
+      const moves = this.nextMoves(curr);
+      moves.forEach(move => {
+        queue.push(move);
+        move.pastMove = curr;
+      });
+    };
+
+    let path = [];
+    while (curr.pastMove) {
+      path.unshift(curr);
+      curr = curr.pastMove;
+    };
+
+    console.log(path);
+    return;
+  }
+
+  nextMoves([x, y], possibleMoves = []) {
+    for (let i = 0; i < Knight.possibleX.length; i++) {
+      let newX = x + Knight.possibleX[i];
+      let newY = y + Knight.possibleY[i];
+      if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+        possibleMoves.push([newX, newY]);
       }
     }
     return possibleMoves;
